f3f85fa6e05b2a6cf9240de2e15c23993ff08c33
KevinCheng1228 <kevin.cheng@advantech.com.tw>
2024-03-26 11:56:03 +0800

    Add files via upload

---
+++ run4.bat
@@ -0,0 +1,11 @@
+java -jar RawdataMigration.jar ^
+mirai.dc_rawdata_20240119-^>mirai ^
+mirai.dc_rawdata_20240120-^>mirai ^
+mirai.dc_rawdata_20240121-^>mirai ^
+mirai.dc_rawdata_20240122-^>mirai ^
+mirai.dc_rawdata_20240123-^>mirai ^
+mirai.dc_rawdata_20240124-^>mirai ^
+mirai.dc_rawdata_20240125-^>mirai ^
+mirai.dc_rawdata_20240126-^>mirai ^
+mirai.dc_rawdata_20240127-^>mirai ^
+mirai.dc_rawdata_20240128-^>mirai
